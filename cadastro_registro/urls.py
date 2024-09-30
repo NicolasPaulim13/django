fb4eee30e416337b5c848e1ea5d394e7d103a79c
NicolasPaulim13 <165830073+NicolasPaulim13@users.noreply.github.com>
2024-09-30 12:30:25 +0000

    mudança day 39 boladão

--- cadastro_registro/urls.py
+++ cadastro_registro/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastro_registro', views.cadastro_registro, name='cadastro_registro'),
+    path('', views.cadastro_registro, name='cadastro_registro'),
     path('create/', views.cadastro_create_paciente, name='cadastro_create_paciente'),
 ]
